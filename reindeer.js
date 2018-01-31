4d03f4ff088e97f5a8160e0bd44509c156e931f0
Arthur Smith <mirandasimpson@Mirandas-Air.attlocal.net>
2018-01-31 17:53:58 -0600

    finished

--- reindeer.js
+++ reindeer.js
@@ -2,13 +2,10 @@ var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwin
 var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
 
 var hohohoElement = document.getElementById("reindeer");
+var text = "";
+for (var i = 0; i < colors.length && i < reindeer.length; i++) {
+        text = colors[i] + reindeer[i];
+        console.log(text);
+};
 
-for (var i = 0; i < colors.length; i++) {
-    var a = colors[i];
-    console.log(a);
-}
-for (var x = 0; x < reindeer.length; x++) {
-    var b = reindeer[x];
-    console.log(b);
-}
-hohohoElement.innerHTML = a + " " + b;
+hohohoElement.innerHTML = text;
